80487923d8e51e09d8c34b2a16126a61b3e62663
itsajay12 <ajayv570040@gmail.com>
2023-12-25 10:20:26 +0530

    updated

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -126,10 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # settings.py
 
-STATIC_ROOT = BASE_DIR / "/home/static"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'home/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home/static')]
+
 
 
 # Default primary key field type
