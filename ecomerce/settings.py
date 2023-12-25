541c86b07da38587dea06c97130199337641fc61
itsajay12 <ajayv570040@gmail.com>
2023-12-25 10:11:14 +0530

    updated

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "/home/static"
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'home/static')]
 
 
 # Default primary key field type
