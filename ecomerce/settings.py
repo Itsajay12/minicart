4339fc705151f729dea9750b66ae4ea55dd18b36
Ajay v <147051273+Itsajay12@users.noreply.github.com>
2023-12-25 13:19:33 +0530

    Update settings.py

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n9dkwhci61b)qtxfnv%p$#aiju681#@bo5fgqlfwu2xi+j7*w)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['minicart-production.up.railway.app',"*"]
 
